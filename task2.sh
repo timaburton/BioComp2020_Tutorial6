d5a6c05a94ce9650da64eddef8568130916e184e
Parinda Tennakoon <pari@parindas-mbp.dhcp.nd.edu>
2020-09-24 18:37:35 -0400

    changes to a script

--- task2.sh
+++ task2.sh
@@ -1,2 +1,5 @@
-cat wages.csv | cut -d , -f 1,2,4 | grep -v 'Gender' | sort -k 3n | head -n 1 
+for i in "*.csv"
+do
+cat wages.csv | cut -d , -f 1,2,4 | grep -v 'Gender' | sort -k 3n | head -n 1  
+done
 
