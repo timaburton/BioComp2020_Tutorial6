fe0bfe1d4afec4c5050078f9b42e0eda22e99fbb
Savannah Miracle <smiracle@nd.edu>
2020-09-24 18:47:01 -0400

    worked on task2

--- task2.sh
+++ task2.sh
@@ -1,2 +1,3 @@
-cat wages.csv | cut -d , -f 1,2,4 | grep -v 'Gender' | sort -k 3n | head -n 1 
-
+cat wages.csv | cut -d , -f 1,2,4 | grep -v "gender" | tr "," " " | sort -k3,3n | head -n1
+cat wages.csv | cut -d , -f 1,2,4 | grep -v "gender" | tr "," " " | sort -k3,3n | tail -n1
+cat wages.csv | cut -d , -f 1,2,4 | grep -v "gender" | tr "," " " | sort -k3,3n | tail | grep -E -c "female" 
