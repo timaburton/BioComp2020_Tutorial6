4170b0e0eca6045e20ece4394ff084419f617c98
Anoop Sunkara <anoopsunkara@anoops-mbp.dhcp.nd.edu>
2020-09-25 02:12:31 -0400

    part 3

--- Exercise6.sh
+++ Exercise6.sh
@@ -20,3 +20,14 @@ Echo “Number of Females in Top Ten Earners”
 cat wages.csv | tr , " " | cut -d " " -f 1,2,4 | sort -k 3n | head -n 11 | tail -n 10 | grep -w "female" | wc -l
 
 #Part3
+
+
+va12=$(cat wages.csv | cut -d , -f 3,4 | tr , " " | grep -E ^12 | cut -d " " -f 2 | awk '{ total += $1 } END { print total/NR }')
+
+
+
+va16=$(cat wages.csv | cut -d , -f 3,4 | tr , " " | grep -E ^16 | cut -d " " -f 2 | awk '{ total += $1 } END { print total/NR }')
+
+Echo Effect of Graduating College on Wages:
+echo "$va16 - $va12" | bc
+
